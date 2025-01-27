a7dd9a12f7b8f350bcfc2249a9745e1273f648cb
mariannemnrt <mariannelerenard@yahoo.fr>
2025-01-27 19:28:55 +0100

    utilisation des input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PlayingCardsComponent } from './playing-cards/playing-cards.component';
+import { Monster } from './models/monster.model';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,16 @@ import { PlayingCardsComponent } from './playing-cards/playing-cards.component';
 })
 export class AppComponent {
   title = 'PlayingCardsAngular';
+
+monster1!:Monster;
+constructor(){
+  this.monster1=new Monster();
+  this.monster1.name="pikachu";
+  this.monster1.hp=70;
+  this.monster1.figureCaption="N°018 Pikachu";
+  this.monster1.attackName="éclair";
+  this.monster1.attackStrength=50;
+  this.monster1.attackDescription="envoie un eclair électrique"
+
+}
 }
---
+++ src/app/models/monster.model.ts
@@ -0,0 +1,8 @@
+export class Monster {
+    name: string="Salamèche";
+    hp: number=120;
+    figureCaption: string="N°027 Salamèche";
+    attackName: string="Brulure";
+    attackStrength: number=60;
+    attackDescription:string="ça fait très chaud this is the complete description of the attack";
+}
