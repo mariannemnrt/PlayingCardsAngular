1917e655300b060f19794d255a431dcf4b2a4051
mariannemnrt <mariannelerenard@yahoo.fr>
2025-01-27 15:25:45 +0100

    creation d'une carte pokemon

---
+++ src/app/playing-cards/playing-cards.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-playing-cards',
+  imports: [],
+  templateUrl: './playing-cards.component.html',
+  styleUrl: './playing-cards.component.css'
+})
+export class PlayingCardsComponent {
+
+}
